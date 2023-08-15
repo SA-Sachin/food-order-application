833f3bae512d6b9784ea544698d349128c8f91e8
sa-sachin <113158970+SA-Sachin@users.noreply.github.com>
2023-08-15 23:50:15 +0530

    updated

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
     <div>
-      
+      <MealsSummary />
+      <AvailableMeals />
     </div>
-  )
-}
+  );
+};
 
-export default Meals
+export default Meals;
